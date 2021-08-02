94d4005defa1043636a5dc1b7f0f5e0ca0ea366c
programmergeek <programergeek@gmail.com>
2021-08-02 21:05:58 +0200

    created click animation for primary button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import { PrimaryButton, SecondaryButton } from "./Components";
 
 const App: React.FC = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {" "}
+      <PrimaryButton buttonName="Login" type="submit" />{" "}
+      <SecondaryButton buttonName="Create an account" />{" "}
+    </div>
+  );
 };
 
 export default App;
