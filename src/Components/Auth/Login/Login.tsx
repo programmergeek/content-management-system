3cb9032ef2e89b614e66b9dca67d9ba2569c539b
programmergeek <programergeek@gmail.com>
2021-08-03 09:31:44 +0200

    added reactive styles

--- src/Components/Auth/Login/Login.tsx
+++ src/Components/Auth/Login/Login.tsx
@@ -22,52 +22,54 @@ export const Login: React.FC = () => {
   return (
     <div className="form-container">
       <div className="form-sub-container">
-        <form onSubmit={handleSubmit(onSubmit)} className="form">
-          <p className="header">Login</p>
-          <label htmlFor="username">Username</label>
-          <input
-            {...register("username", { required: true, maxLength: 100 })}
-            type="text"
-            className="field"
-          />
-          {errors.username && errors.username?.type === "required" && (
-            <p className="error-message"> You forgot this. </p>
-          )}
-          {errors.username && errors.username?.type === "maxLength" && (
-            <p className="error-message">
-              {" "}
-              Your username is just a bit too long there.{" "}
-            </p>
-          )}
+        <div className="form">
+          <form onSubmit={handleSubmit(onSubmit)} className="action-area">
+            <p className="header">Login</p>
+            <label htmlFor="username">Username</label>
+            <input
+              {...register("username", { required: true, maxLength: 100 })}
+              type="text"
+              className="field"
+            />
+            {errors.username && errors.username?.type === "required" && (
+              <p className="error-message"> You forgot this. </p>
+            )}
+            {errors.username && errors.username?.type === "maxLength" && (
+              <p className="error-message">
+                {" "}
+                Your username is just a bit too long there.{" "}
+              </p>
+            )}
 
-          <label htmlFor="password">Password</label>
-          <input
-            {...register("password", {
-              required: true,
-              maxLength: 100,
-              minLength: 8,
-            })}
-            type="password"
-            className="field"
-          />
-          {errors.password && errors.password?.type === "required" && (
-            <p className="error-message"> You forgot this. </p>
-          )}
-          {errors.password && errors.password?.type === "maxLength" && (
-            <p className="error-message">
-              {" "}
-              That password might be a bit too secure.{" "}
-            </p>
-          )}
-          {errors.password && errors.password?.type === "minLength" && (
-            <p className="error-message"> A bit too short. </p>
-          )}
+            <label htmlFor="password">Password</label>
+            <input
+              {...register("password", {
+                required: true,
+                maxLength: 100,
+                minLength: 8,
+              })}
+              type="password"
+              className="field"
+            />
+            {errors.password && errors.password?.type === "required" && (
+              <p className="error-message"> You forgot this. </p>
+            )}
+            {errors.password && errors.password?.type === "maxLength" && (
+              <p className="error-message">
+                {" "}
+                That password might be a bit too secure.{" "}
+              </p>
+            )}
+            {errors.password && errors.password?.type === "minLength" && (
+              <p className="error-message"> A bit too short. </p>
+            )}
 
-          <PrimaryButton buttonName="Login" type="submit" />
+            <PrimaryButton buttonName="Login" type="submit" />
+          </form>
           <a href="/signup" className="auth-page-link">
             <SecondaryButton buttonName="Sign Up" />
           </a>
-        </form>
+        </div>
       </div>
       <div className="colour-gradient"></div>
     </div>
