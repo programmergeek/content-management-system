7ad57d350729e818aaffd2bde00d25a78f900930
programmergeek <programergeek@gmail.com>
2021-08-06 15:47:33 +0200

    created custom typed hooks meant to replace useDispatch and useSelector

---
+++ src/Components/Hooks/ReduxHooks.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { RootState, AppDispatch } from "../../Redux/store";
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,3 +1,4 @@
 export * from "./Button/Button";
 export * from "./Auth/Login/Login";
 export * from "./Auth/SignUp/SignUp";
+export * from "./Hooks/ReduxHooks";
