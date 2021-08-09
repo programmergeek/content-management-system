0eff2da76e39e4998f76f1d31965a44909299fa0
programmergeek <programergeek@gmail.com>
2021-08-09 18:48:00 +0200

    created slider component

---
+++ src/Components/Slider/Slider.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Slider: React.FC = () => {
+  return <div></div>;
+};
--- src/Components/index.ts
+++ src/Components/index.ts
@@ -4,3 +4,4 @@ export * from "./Auth/SignUp/SignUp";
 export * from "./Hooks/ReduxHooks";
 export * from "./Charts/Charts";
 export * from "./Table/Table";
+export * from "./Slider/Slider";
